cd7b619d4bd3853629e7e484c5d5daf02e322078
olivierkoe <olikoen@gmail.com>
2023-09-05 11:03:37 +0200

    2 git ajour

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <h2>REACT BRAINS 4Thunes 2023 - 2024</h2>
-            <p>Copie </p>
+            <p>Copie de EXPRESS BRAIN de l'ENI </p>
         </div>
     )
 }
